5fe87374240693c7fc79ff12709647e330ac207e
JNaeder <39416919+JNaeder@users.noreply.github.com>
2022-02-28 23:15:12 -0500

    Added two knobs and colored them.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,22 @@
 MyTest2282022AudioProcessorEditor::MyTest2282022AudioProcessorEditor (MyTest2282022AudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+    addAndMakeVisible(frequency_knob);
+    frequency_knob.setSliderStyle(Slider::Rotary);
+    frequency_knob.setRange(0.0, 20'000.0, 1.0);
+    frequency_knob.setTextBoxStyle(Slider::NoTextBox, true, 100, 20);
+    frequency_knob.setValue(20'000.0);
+
+    addAndMakeVisible(cutoff_knob);
+    cutoff_knob.setSliderStyle(Slider::Rotary);
+    cutoff_knob.setRange(0.0, 10.0, 0.01);
+    cutoff_knob.setTextBoxStyle(Slider::NoTextBox, true, 100, 20);
+    cutoff_knob.setValue(0.0);
+
+    getLookAndFeel().setColour(Slider::thumbColourId, Colours::indianred);
+    getLookAndFeel().setColour(Slider::rotarySliderFillColourId, Colours::antiquewhite);
 }
 
 MyTest2282022AudioProcessorEditor::~MyTest2282022AudioProcessorEditor()
@@ -25,16 +38,14 @@ MyTest2282022AudioProcessorEditor::~MyTest2282022AudioProcessorEditor()
 //==============================================================================
 void MyTest2282022AudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll(Colours::transparentBlack);
 }
 
 void MyTest2282022AudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    int border = 40;
+    int knobWidth = getWidth() / 2 - border;
+    int knobHeight = getHeight() - 40;
+    frequency_knob.setBounds(border, border, knobWidth, knobHeight);
+    cutoff_knob.setBounds(getWidth() / 2, border, knobWidth, knobHeight);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,6 +28,7 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     MyTest2282022AudioProcessor& audioProcessor;
-
+    Slider frequency_knob;
+    Slider cutoff_knob;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MyTest2282022AudioProcessorEditor)
 };
